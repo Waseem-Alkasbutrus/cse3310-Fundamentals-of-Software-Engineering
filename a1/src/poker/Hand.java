ef39df9a6707b8106a2e6e400dba1d154aee1bf6
Waseem Alkasbutrus <waseem_imad@yahoo.com>
2022-02-01 23:44:41 -0600

    Added Hand.toString

--- a1/src/poker/Hand.java
+++ a1/src/poker/Hand.java
@@ -6,7 +6,7 @@ import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 
 public class Hand {
-    //private transient int i=10;
+    // private transient int i=10;
     // marked transient they will not serialized / deserialized
 
     public Card[] cards;
@@ -19,16 +19,27 @@ public class Hand {
             throw new Exception("There must be 5 cards in a hand (provided " + cards.length + " cards)");
         }
 
-        
+        for (int i = 0; i < 5; i++) {
+            this.cards[i] = cards[i];
+        }
     }
 
     public boolean is_better_than(Hand H) {
         return false;
     }
 
+    public boolean is_equal(Hand H) {
+        return false;
+    }
 
-   public boolean is_equal(Hand H)
-   {
-       return false;
-   }
+    @Override
+    public String toString() {
+        StringBuilder hand = new StringBuilder();
+
+        for (Card c : this.cards) {
+            hand.append(c.toString() + "\n");
+        }
+
+        return hand.toString();
+    }
 }
