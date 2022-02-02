34b889d20d60b2d69ee4e8e64a3efa1ffd6a497a
Waseem Alkasbutrus <waseem_imad@yahoo.com>
2022-02-02 00:10:35 -0600

    Changed the way to hands are initialized and cloned, and changed how cards are initialized too

--- a1/src/Main.java
+++ a1/src/Main.java
@@ -1,72 +1,60 @@
-import com.google.gson.Gson; 
-import com.google.gson.GsonBuilder; 
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
 import poker.Hand;
 import poker.Card.Suite;
 import poker.Card.Value;
 import poker.Card;
 
-
 public class Main {
 
-  public static void main(String[] args) {
-     // this function is provided as a convenient way to
-     // run the code from the command line.
-     //
-     // it will not be graded, but it needs to compile and
-     // execute without a failure.
+   public static void main(String[] args) {
+      // this function is provided as a convenient way to
+      // run the code from the command line.
+      //
+      // it will not be graded, but it needs to compile and
+      // execute without a failure.
 
-     System.out.println("==================================\nStart");
+      System.out.println("==================================\nStart");
 
-     // Demonstrate converting json in and out of a 'Hand'
-     //
-     Hand H1 = new Hand();
+      // allocate space for 5 card references
+      Card[] cards = new Card[5];
+      cards[0] = new Card(Value.FIVE, Suite.SPADES);
+      cards[1] = new Card(Value.FOUR, Suite.SPADES);
+      cards[2] = new Card(Value.THREE, Suite.SPADES);
+      cards[3] = new Card(Value.TWO, Suite.SPADES);
+      cards[4] = new Card(Value.ACE, Suite.SPADES);
 
-     // allocate space for 5 card references
-     H1.cards = new Card[5];
-     H1.cards[0] = new Card();
-     H1.cards[0].suite = Card.Suite.valueOf("SPADES");
-     H1.cards[0].value = Card.Value.valueOf("FIVE");
-     H1.cards[1] = new Card();
-     H1.cards[1].suite = Card.Suite.valueOf("SPADES");
-     H1.cards[1].value = Card.Value.valueOf("FOUR");
-     H1.cards[2] = new Card();
-     H1.cards[2].suite = Card.Suite.valueOf("SPADES");
-     H1.cards[2].value = Card.Value.valueOf("THREE");
-     H1.cards[3] = new Card();
-     H1.cards[3].suite = Card.Suite.valueOf("SPADES");
-     H1.cards[3].value = Card.Value.valueOf("TWO");
-     H1.cards[4] = new Card();
-     H1.cards[4].suite = Card.Suite.valueOf("SPADES");
-     H1.cards[4].value = Card.Value.valueOf("ACE");
+      // Demonstrate converting json in and out of a 'Hand'
+      //
+      Hand H1 = new Hand(cards);
 
-     // turn it into json
-     Gson gson = new Gson();
-     String jsonString = gson.toJson(H1);
-     System.out.println(jsonString);
+      // turn it into json
+      Gson gson = new Gson();
+      String jsonString = gson.toJson(H1);
+      System.out.println(jsonString);
 
-     // and back into a "hand"
-     Hand H3 = new Hand();
-     H3 = gson.fromJson(jsonString, Hand.class);
+      // and back into a "hand"
+      Hand H3 = new Hand();
+      H3 = gson.fromJson(jsonString, Hand.class);
 
-     // change something
-     H3.cards[4].suite = Card.Suite.valueOf("DIAMONDS");
-     H3.cards[4].value = Card.Value.valueOf("FIVE");
+      // change something
+      H3.swapCard(4, new Card(Value.FIVE, Suite.DIAMONDS));
 
-     // show that it works
-     System.out.println(gson.toJson(H3));
+      // show that it works
+      System.out.println(gson.toJson(H3));
 
-     // show the comparison function
-     Hand H2 = new Hand();
-     H2 = H1;
+      // show the comparison function
+      Hand H2 = H1.duplicate();
 
-     if (H1.is_better_than(H2)) {
-        System.out.println("hand1 is better than hand2");
-     }
+      if (H1.is_better_than(H2)) {
+         System.out.println("hand1 is better than hand2");
+      }
 
-     if (H2.is_better_than(H1)) {
-        System.out.println("hand2 is better than hand1");
-     }
+      if (H2.is_better_than(H1)) {
+         System.out.println("hand2 is better than hand1");
+      }
 
-     System.out.println("End\n==================================");
-  }
+      System.out.println("End\n==================================");
+   }
 }
